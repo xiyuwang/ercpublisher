37fe7da5ebd7746489edb40eb8e8105119992aa2
Xiyu Wang <wang.xy@hotmail.com>
2018-05-01 21:22:10 +0800

    the web framework

---
+++ dapp/src/main/java/AppController.java
@@ -0,0 +1,16 @@
+package com.ercpublisher.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String handleError(){
+        return "index"; 
+    }
+
+}
+
